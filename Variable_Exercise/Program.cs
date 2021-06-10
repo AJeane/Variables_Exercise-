710b3838ab3c3b87e3add3208005d584cf8cc05d
AJeane <ajeane1924@gmail.com>
2021-06-10 10:51:55 -0500

    initial commit

---
+++ Variable_Exercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Variable_Exercise
+{
+    class Program
+    {
+        private const string R = "R";
+
+        static void Main(string[] args)
+        {
+
+            string nameDog = "Rico";
+
+            int dogAge = 12;
+
+            char initialName = 'R';
+
+            bool constantlyAround = true;
+
+            double walksNeeded = 1;
+
+            decimal height = 0.24m;
+
+            Console.WriteLine($"My dog's name is {nameDog}, He is {dogAge} years old");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
